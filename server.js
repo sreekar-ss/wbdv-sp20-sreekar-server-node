f31215be697fbac0fbef65a0f4adda7ee7e2cf6f
Sai Sreekar <siddula.s@husky.neu.edu>
2020-04-13 03:42:55 -0400

    Heroku Cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200, https://wbdv-sp20-sreekar-angular-c.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', 'https://wbdv-sp20-sreekar-angular-c.herokuapp.com');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
